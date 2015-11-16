5a70d976804c1e6c411e0a8aa7cc7646668a1d2c
Lydiamnash <lydiamnash@gmail.com>
2015-11-15 20:32:32 -0800

    Adds directories and files for 4.5

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,23 @@
+# Calculate a Grade
+
+# I worked on this challenge Joseph Scott.
+
+
+# Your Solution Below
+
+def get_grade average
+	if average >= 90
+		return 'A'
+	elsif 
+		(average >= 80 && average < 90)
+			return 'B'
+		elsif
+			(average >=70 && average <80)
+				return 'C'
+			elsif 
+				(average >=60 && average <70)
+					return 'D'
+				else
+					return 'F'		
+	end
+end
